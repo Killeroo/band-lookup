3f48ba7f4cdb602edf37caa9df148dde8d3d12be
Killeroo <matthewcarney64@gmail.com>
2017-08-14 17:48:31 +0100

    Added react-bootstrap Added searchbar component

--- src/App.js
+++ src/App.js
@@ -2,17 +2,32 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function SearchBar() {
+  return (
+    <div className="col-lg-6">
+      <div className="input-group">
+        <input type="text" className="form-control" placeholder="Search for..." />
+        <span className="input-group-btn">
+          <button className="btn btn-secondary" type="button">Search</button>
+        </span>
+      </div>
+    </div>
+  )
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to Band Lookup</h2>
+          <p>Get info on any band...</p>
         </div>
         <p className="App-intro">
-          Meowwww
+
         </p>
+        <SearchBar />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -4,20 +4,5 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-function clock() {
-	// Define our element
-	const element = (
-		<div>
-			<p>The time is {new Date().toLocaleTimeString()}.</p>
-		</div>
-	)
-	// Render in DOM
-	ReactDOM.render(
-		element,
-		document.getElementById('clock')
-	)
-}
-
-setInterval(clock, 1000);
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
