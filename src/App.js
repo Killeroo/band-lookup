7b387aae04b91ba64f0d8bc4e217fa25bdee4f85
Killeroo <matthewcarney64@gmail.com>
2017-08-15 01:23:24 +0100

    Added results list

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
-const parseString = require('react-native-xml2js').parseString; // Xml parser
+
+const parseString = require('react-native-xml2js').parseString; // Xml converter
+var xmldoc = require('xmldoc'); // Xml doc parser
 
 class SearchBar extends Component {
   handleSearchClick = () => {
     const name = this._name.value;
-    console.log(name);
 
-    // Make call to MusicBrainz search api
-    fetch('http://musicbrainz.org/ws/2/artist/?query=artist:fred%20AND%20type:group%20AND%20country:US')
-      .then(response => response.text()) // Convert response to text
+    // Query MusicBrainz search api
+    fetch('http://musicbrainz.org/ws/2/artist/?query=artist:' + name)
+      // Convert response to text
+      .then(response => response.text()) 
       .then((response) => {
-        parseString(response, function (err, result) { // Convert text to xml string
-          console.log(response);
-          
+        // Convert text to xml string
+        parseString(response, function (err, result) { 
+
+          // Process our response
+          var xmlResponse = new xmldoc.XmlDocument(response); 
+          var artists = xmlResponse.childNamed('artist-list').children;
+          var len = artists.length;
+          var results = [];
+          var listItems;
+
+          console.log(artists);
+
+          // Loop through found artists
+          for (var x = 0; x < len; x++) {
+            var artist = artists[x].childNamed('name');
+            var name = artist.val;
+            results.push(name);
+          }
+
+          // Map results to html list
+          listItems = results.map((result) =>
+            <li>{result}</li>
+          );
+
+          // Render list in ReactDOM
+          ReactDOM.render(<ul>{listItems}</ul>, document.getElementById('result_list'))
+
         });
       }).catch((err) => {
         console.log('fetch', err); 
-      })
+    });
   }
 
   render() {
@@ -35,7 +62,6 @@ class SearchBar extends Component {
   }
 }
 
-// Top level react component
 class App extends Component {
   render() {
     return (
